0b0820af0a35aa8fbcb4296556b976a1f47e99b7
JasonCB083 <jason.cllr@gmail.com>
2020-07-28 15:57:50 +0200

    animated popup final code

---
+++ main.js
@@ -0,0 +1,18 @@
+const modal = document.querySelector("#modal");
+const modalBtn = document.querySelector("#modal-btn");
+const ans = document.querySelector("#answer");
+
+modalBtn.addEventListener("click", () => {
+  modal.className = "open";
+})
+
+modal.addEventListener("click", (e)=> {
+  if (e.target.id === "yes-btn") {
+    ans.innerText =  " yay! you're Pawsome "
+  } else if (e.target.id === "no-btn") {
+    ans.innerText = " Oh no! Grrr the Boyz are angry "
+  } else {
+    return;
+  }
+  modal.className = "close";
+})
